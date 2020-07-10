5e997845809f55ebacf9bee15bad34450ec322ed
AJ Snarr <ajsnarr98@gmail.com>
2020-07-10 12:28:17 -0400

    simplified bot.py

--- bot.py
+++ bot.py
@@ -23,39 +23,55 @@ handler = logging.FileHandler(filename=log_filename, encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-
-class DiscordBot(commands.Bot):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.default_command_prefix = kwargs.get('default_command_prefix', None)
-
-    async def on_ready(self):
-        logger.info('Connected!')
-        logger.info('Username: {}'.format(self.user.name))
-        logger.info('ID: {}'.format(self.user.id))
-        
-        # self.load_extension('extensions.core')
-
-    async def on_command_error(self, exception, context):
-        """ Logs and ignores errors in commands, unless that exception was from
-            entering an invalid command, in which case this instead tells the
-            user that they gave an invalid command.
-        """
-        if type(exception) == commands.errors.CommandNotFound:
-            await self.send_message(context.message.channel, str(exception))
-        elif type(exception) == commands.errors.MissingRequiredArgument:
-            await self.send_message(context.message.channel,
-                'Missing required argument. Please see \'{}help\'.'.format(self.default_command_prefix))
-        else:
-            logger.error('Ignoring exception in command {}'.format(context.command))
-            print('Ignoring exception in command {}'.format(context.command), file=sys.stderr)
-            traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
-
+# set up bot
 default_command_prefix = '!'
 
+description = ''' A bot to fulfill your wildest dreams. '''
+bot = commands.Bot(command_prefix=default_command_prefix,
+  description=description, pm_help=False)
+
+### Utility Functions ###
+
+
+### Event handlers ###
+
+@bot.event
+async def on_connect():
+  logger.info('Connected!')
+  logger.info('Username: {}'.format(bot.user.name))
+  logger.info('ID: {}'.format(bot.user.id))
+
+@bot.event
+async def on_command_error(ctx, exception):
+  """ Logs and ignores errors in commands, unless that exception was from
+      entering an invalid command, in which case this instead tells the
+      user that they gave an invalid command.
+  """
+  if type(exception) == commands.errors.CommandNotFound:
+    await ctx.send(str(exception))
+  elif type(exception) == commands.errors.MissingRequiredArgument:
+    await ctx.send(
+      'Missing required argument. Please see \'{}help\'.'.format(default_command_prefix))
+  else:
+    logger.error('Ignoring exception in command {}'.format(context.command))
+    print('Ignoring exception in command {}'.format(context.command), file=sys.stderr)
+    traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
+
+### Commands ###
+
+@bot.command()
+async def gradyear(ctx, year):
+  """ Set your graduation year. '!gradyear <year>' """
+  n_year = 0
+  try:
+    n_year = int(year)
+  except ValueError:
+    await ctx.send('\'{}\' is not a number'.format(year))
+    return
+  
+  await ctx.send('hello')
+  
+
 if __name__ == '__main__':
-    description = ''' A bot to fulfill your wildest dreams. '''
-    logger.info('connecting...')
-    bot = DiscordBot(command_prefix=default_command_prefix, description=description, pm_help=False)
-    bot.run(secret.botToken)
+  logger.info('connecting...')
+  bot.run(secret.botToken)
